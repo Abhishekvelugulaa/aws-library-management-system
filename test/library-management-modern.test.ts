f4678f36060c1be01d21d967d39774163c2f6b71
Abhishek <abhishek78168@gmail.com>
2026-02-11 00:02:55 +0530

    Initial commit

---
+++ test/library-management-modern.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LibraryManagementModern from '../lib/library-management-modern-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/library-management-modern-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LibraryManagementModern.LibraryManagementModernStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
